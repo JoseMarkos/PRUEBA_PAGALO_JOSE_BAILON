8ca9c305cd243312c135bc4eabf88653f32ad466
jose <jkmarcosb@gmail.com>
2025-03-30 21:35:25 -0600

    agregar rutas de listas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './components/movie/list/list.component';
+import { NowPlayingListComponent } from './components/movie/now-playing-list/now-playing-list.component';
+import { TopRatedListComponent } from './components/movie/top-rated-list/top-rated-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        'path' : 'en-cartelera',
+        'component' : NowPlayingListComponent
+    },
+    {
+        'path' : 'mas-populares',
+        'component' : TopRatedListComponent
+    },
+];
